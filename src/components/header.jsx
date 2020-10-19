5fceeed31da84bd4948ab45a99817465eaf9e24d
Wojciech Rogoz <wrogoz@gmail.com>
2020-10-19 16:14:03 +0200

    temporary logout btn with functionality added

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import styled from 'styled-components'
+import {connect} from 'react-redux';
 
-const Header = ()=>{
-    const clear = ()=>{
+const Header = (props)=>{
+    const logoutHandler = ()=>{
         localStorage.removeItem('access-token')
+        props.dispatch({type:"LOGOUT"})
     }
     return(
         <HeaderContainer>
-            <h1 onClick={clear}>QuickShop</h1>
+            <h1>QuickShop</h1>
+            {props.isUserLoggedIn?<div onClick={logoutHandler}>logout</div>:null}
         </HeaderContainer>
     )
 }
@@ -19,8 +22,18 @@ const HeaderContainer = styled.header`
   align-items:center;
   justify-content:center;
   color:#fff;
+  position: relative;
+  div{
+      position:absolute;
+      top:10px;
+      right:10px;
+  }
   
 `;
+const mapStateToProps = (state, ownProps) => ({
+    test: state.test,
+    isUserLoggedIn: state.isUserLoggedIn,
+    shoppingCart:state.shoppingCart
+  });
+  export default connect(mapStateToProps)(Header)
 
-
-export default Header
