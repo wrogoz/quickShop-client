d5946efe5add54f9464b13d2927108d9c8013794
Wojciech Rogoz <wrogoz@gmail.com>
2020-11-02 12:21:41 +0100

    client side rendering issue

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,37 +1,40 @@
-
 const initialState = {
-    test:'reducer test',
-    isUserLoggedIn:true,
-    shoppingCart:[]
-   }
-  
-  const reducer = (state = initialState, action)=> {
-      switch (action.type) {
-     
-        case 'LOGIN':
-          return{
-            ...state,
-            isUserLoggedIn:true
-          }
-          case 'LOGOUT':
-          return{
-            ...state,
-            isUserLoggedIn:false
-          }
-      
-    
-    case 'EDITSHOPPINGCART':
-          return{
-            ...state,
-            shoppingCart:action.shoppingCart
-          }
-          case 'RESETSHOPPINGCART':
-            return{
-              ...state,
-              shoppingCart:[]
-            }
-          
-          default:
-            return state
-        }}
-    export default reducer;
+  test: "reducer test",
+  isUserLoggedIn: true,
+  shoppingCart: [],
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      return {
+        ...state,
+        isUserLoggedIn: true,
+      };
+    case "LOGOUT":
+      return {
+        ...state,
+        isUserLoggedIn: false,
+      };
+
+    case "EDITSHOPPINGCART":
+      return {
+        ...state,
+        shoppingCart: action.shoppingCart,
+      };
+    case "DELETEPRODUCT":
+      return {
+        state,
+        shoppingCart: action.shoppingCart,
+      };
+    case "RESETSHOPPINGCART":
+      return {
+        ...state,
+        shoppingCart: [],
+      };
+
+    default:
+      return state;
+  }
+};
+export default reducer;
